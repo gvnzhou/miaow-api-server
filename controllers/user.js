176bc904cfd5bc65019ee701e3aca1f266b4f118
javion <228806684@qq.com>
2017-04-17 16:05:07 +0800

    update

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,8 @@
 /**
  * Created by h9zhou on 2017/4/12.
  */
-const UserModel = require('../models/user');
+const errorMsg = require('../core/errorMsg')
+const model = require('../models/model');
 
 const UserController = {};
 
@@ -23,24 +24,55 @@ UserController.signup = async function (ctx) {
 	let nickname = ctx.request.body.nickname || `喵星人_${Math.floor(Math.random() * 100000000)}`; // 不设置就随机生成
 	let email = ctx.request.body.email || '';
 	let avatar = 'http://7xkwup.com1.z0.glb.clouddn.com/20120815170312_cyZui.jpeg'; // 默认头像地址
+	let sex = ctx.request.body.sex || 0;
 	let signature = '这个用户很勤快，什么都留下了。';
-	let regTime = +new Date();
-	let loginTime = +new Date();
+	let reg_time = +new Date() + '';
+	let login_time = +new Date();
+
+	// const res = await model.user.findAll();
+	// ctx.body = res;
+	// return;
+
+	let res = await model.User
+		.findOrCreate({
+			where: {
+				mobile,
+			},
+			defaults: {
+				mobile,
+				password,
+				nickname,
+				email,
+				avatar,
+				sex,
+				signature,
+				reg_time,
+				login_time,
+			},
+		});
+	// .spread((user, created) => {
+	// 	console.log(user.get({
+	// 		plain: true,
+	// 	}))
+	// 	console.log(created);
+	// });
+	console.log(res);
 
 	// 手机号是否正确
-	if (mobile.length !== 11) {
+	if (!(/^1(3|4|5|7|8)\d{9}$/.test(mobile))) {
+		ctx.body = errorMsg.MOBILE_RULE_ERROR;
+		return;
 	}
 
-	const res = await UserModel.findAll();
-	// const sql = 'SELECT * FROM mw_user'; // 插入sql
-	// const res = await query(sql);
+	// 密码格式是否正确
+	if (password.length > 6) {
+		ctx.body = errorMsg.PASSWORD_ERROR;
+		return;
+	}
 
 	// token
-	// 区别手机号
 	// 手机号是否存在
-	// 手机号是否正确
-	// 密码是否符合规则
-	ctx.body = nickname;
+	// ctx.body = ctx.request;
 }
 
 UserController.sendVerifyCode = async function (ctx) {}
---
+++ core/errorMsg.js
@@ -0,0 +1,19 @@
+/**
+ * Created by h9zhou on 2017/4/17.
+ */
+const errorMsg = {
+	MOBILE_RULE_ERROR: {
+		code: '10001',
+		message: '手机号格式错误！',
+	},
+	MOBILE_EXISTS_ERROR: {
+		code: '10002',
+		message: '手机号已存在！',
+	},
+	PASSWORD_ERROR: {
+		code: '10003',
+		message: '密码格式错误！',
+	},
+};
+
+module.exports = errorMsg;
